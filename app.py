45b34c181904bdea34498f532efb0b892675efde
Shubham Surve <118058459+shub-surve@users.noreply.github.com>
2024-09-30 11:12:34 +0530

    Update app.py --added dynamic web

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template , jsonify
+
 app = Flask(__name__)
+
+JOBS = [
+    {"id": 1, "title": "Software Engineer","Location":"Mumbai", "description": "Develop software applications"},
+    {"id": 2, "title": "Data Scientist","Location":"Pune", "description": "Analyze and interpret data"},
+    {"id": 3, "title": "DevOps Engineer","Location":"Hydrabad", "description": "Ensure smooth operation of systems"},
+    {"id": 3, "title": "Software Engineer","Location":"Bangalore", "description": "Ensure smooth operation of systems"}
+]
 
 @app.route("/")
 def index():
-    return "Hii Welcome to IntApply"
+    return render_template('home.html', jobs=JOBS)
 
-
-
+@app.route('/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
